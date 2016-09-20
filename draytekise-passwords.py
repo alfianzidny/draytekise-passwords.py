6cccc7f946795085db7b657ad029fe706ddd2ea8
Ste Martin <sudormminusrf@gmail.com>
2016-09-21 00:15:33 +0100

    Update draytekise-passwords.py

--- draytekise-passwords.py
+++ draytekise-passwords.py
@@ -5,5 +5,5 @@ import urllib
 
 
 with open(sys.argv[1]) as f:
-	for line in f.readlines():
-		print urllib.quote_plus(base64.b64encode(line.strip()))
+        for line in f.readlines():
+                print base64.b64encode(line.strip())
